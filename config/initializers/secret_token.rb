aa89ffed351907fd097446e57aad2f831ebfeb67
Ilya Korolev <aleksandrich@gmail.com>
2013-03-13 20:16:12 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stdev::Application.config.secret_token = 'a87d9248a13ec9266480a3ca95c37beee1db5e9d21e73c3d24bc4ceafe09be98d73da677bcf484d7bcc6cd119ed72ce6273f6e8c450dfb148c797439cafc58a7'
